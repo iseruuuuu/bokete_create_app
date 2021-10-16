9db5de248c59a7ea431829116d4088b0cfc63ec8
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-10-16 21:29:57 +0900

    GetXを使用したアプリです。

---
+++ android/app/src/main/kotlin/com/example/bokete_create_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bokete_create_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screen/home/home_screen_controller.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomeScreenController extends StatelessWidget {
+  const HomeScreenController({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
