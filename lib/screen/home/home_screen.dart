ac00fc6383aae328f73da9b04fbb4a857eeb5005
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-10-17 10:39:37 +0900

    add: 画像を追加

--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:bokete_create_app/gen/assets.gen.dart';
 import 'package:bokete_create_app/screen/home/home_screen_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -5,10 +6,28 @@ import 'package:get/get.dart';
 class HomeScreen extends StatelessWidget {
   HomeScreen({Key? key}) : super(key: key);
 
-  final controller = Get.put(HomeScreenController());
+  //final controller = Get.put(HomeScreenController());
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          Center(
+            child: GestureDetector(
+              onTapDown: (d) {
+                //TODO 後で登録
+              },
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.width,
+                child: Assets.images.background.image(),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
