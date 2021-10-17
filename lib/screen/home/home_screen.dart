dc8fc2525c2ffea37c4d3d62ac17ceedd7491687
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-10-17 10:42:38 +0900

    fix: change sizedBox

--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -22,7 +22,7 @@ class HomeScreen extends StatelessWidget {
               onTapDown: (d) {
                 //TODO 後で登録
               },
-              child: Container(
+              child: SizedBox(
                 width: MediaQuery.of(context).size.width,
                 height: MediaQuery.of(context).size.width,
                 child: Assets.images.background.image(),
