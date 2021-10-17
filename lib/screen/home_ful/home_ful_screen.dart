0b14d55d2705c415e080c851ae182a93f6fbd9f9
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-10-17 22:23:07 +0900

    add: 画像を保存できるようにした

--- lib/screen/home_ful/home_ful_screen.dart
+++ lib/screen/home_ful/home_ful_screen.dart
@@ -16,14 +16,7 @@ class _HomeFulScreenState extends State<HomeFulScreen> {
   ImageProvider? target;
   String fontName = '';
   String assets = 'assets/images/odai.png';
-
-  //TODO やること
-
-  /*
-  ・画像の保存
-
-  ・文字数によってOffsetを変えてあげる。
-   */
+  late Uint8List _uint8list;
 
   @override
   Widget build(BuildContext context) {
@@ -79,9 +72,34 @@ class _HomeFulScreenState extends State<HomeFulScreen> {
     const int size = 60;
     final ImageEditorOption option = ImageEditorOption();
     final AddTextOption textOption = AddTextOption();
+    var position = Offset(0, 0);
+    if (_controller.text.isEmpty) {
+      position = const Offset(0, 0);
+    } else if (_controller.text.length == 1) {
+      position = Offset(330, 240);
+    } else if (_controller.text.length == 2) {
+      position = Offset(300, 240);
+    } else if (_controller.text.length == 3) {
+      position = Offset(270, 240);
+    } else if (_controller.text.length == 4) {
+      position = Offset(250, 240);
+    } else if (_controller.text.length == 5) {
+      position = Offset(220, 240);
+    } else if (_controller.text.length == 6) {
+      position = Offset(190, 240);
+    } else if (_controller.text.length == 7) {
+      position = Offset(170, 240);
+    } else if (_controller.text.length == 8) {
+      position = Offset(150, 240);
+    } else if (_controller.text.length == 9) {
+      position = Offset(130, 240);
+    } else if (_controller.text.length == 10) {
+      position = Offset(100, 240);
+    }
     textOption.addText(
       EditorText(
-        offset: const Offset(80, 240),
+        //offset: const Offset(80, 240),
+        offset: position,
         text: _controller.text,
         fontSizePx: size,
         textColor: Colors.black,
@@ -99,6 +117,8 @@ class _HomeFulScreenState extends State<HomeFulScreen> {
       return;
     }
     target = MemoryImage(result);
+    _uint8list = result;
+
     setState(() {});
   }
 
@@ -108,32 +128,6 @@ class _HomeFulScreenState extends State<HomeFulScreen> {
   }
 
   void onImageStore() async {
-    // var response = await Dio().get(target.toString(),
-    //     options: Options(responseType: ResponseType.bytes));
-    // final result = await ImageGallerySaver.saveImage(
-    //     Uint8List.fromList(response.data),
-    //     quality: 60,
-    //     name: "hello");
-
-    //Uint8List _buffer = await
-
-    //ImageProvider<Object>? target
-    //Uint8List _buffer =  await rootBundle.load(target).buffer
-    // var _buffer = await rootBundle.load('')..buffer.asUint8List();
-    // final result = await ImageGallerySaver.saveImage(_buffer);
-
-    // (await rootBundle.load(target)).buffer.asUint8List();
-
-    // ByteData imageData;
-    // rootBundle.load('assets/test.jpg')
-    //     .then((data) => setState(() => this.imageData = data));
-
-    // var data = await rootBundle.load(target.toString());
-    // setState(() => this.imageData = data);
-
-    final ByteData bytes = await rootBundle.load(target.toString());
-    final Uint8List list = bytes.buffer.asUint8List();
-
-    final result = await ImageGallerySaver.saveImage(list);
+    final result = await ImageGallerySaver.saveImage(_uint8list);
   }
 }
