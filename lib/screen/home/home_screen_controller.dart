cbaa2f1ffbf2137aee4fd186482e550f0e05ba77
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-10-17 11:54:58 +0900

    add: 文字を反映する関数を追加

--- lib/screen/home/home_screen_controller.dart
+++ lib/screen/home/home_screen_controller.dart
@@ -1,67 +1,32 @@
 import 'dart:io';
-
 import 'dart:typed_data';
+import 'package:bokete_create_app/gen/assets.gen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:image_editor/image_editor.dart';
 import 'package:get/get.dart';
 
 class HomeScreenController extends GetxController {
-  ImageProvider? image;
-  BlendMode blendMode = BlendMode.srcOver;
-  File? _selectedImage;
+  final textOption = AddTextOption();
+  final images = Assets.images.background.image();
 
   @override
   void onInit() {
     super.onInit();
-    // Future.delayed(Duration.zero, () {
-    //   _selectedImage = ModalRoute.of(context).settings.arguments;
-    //   image = MemoryImage(_selectedImage?.readAsBytesSync());
-    //   //setState(() {});
-    // });
-  }
-
-  // Future<void> mixImage(
-  //     BuildContext context, File srcImg, TapDownDetails details) async {
-  //   final Uint8List src = srcImg.readAsBytesSync();
-  //   final Uint8List material = await loadFromAsset(ImgPaths.SUNGLASS_PNG);
-  //   final ImageEditorOption optionGroup = ImageEditorOption();
-  //
-  //   final Image img = Image.file(srcImg);
-  //   print('${img.width.toString()}, ${img.height.toString()}');
-  //
-  //   RenderBox getBox = context.findRenderObject();
-  //
-  //   var localPos = getBox.globalToLocal(details.globalPosition);
-  //   int x = localPos.dx.toInt();
-  //   int y = localPos.dy.toInt();
-  //
-  //   print("$x,$y");
-  //
-  //   optionGroup.outputFormat = const OutputFormat.png();
-  //   optionGroup.addOption(
-  //     MixImageOption(
-  //       x: x,
-  //       y: y,
-  //       width: 150,
-  //       height: 150,
-  //       target: MemoryImageSource(material),
-  //       blendMode: blendMode,
-  //     ),
-  //   );
-  //   final Uint8List? result =
-  //   await ImageEditor.editImage(image: src, imageEditorOption: optionGroup);
-  //   image = MemoryImage(result!);
-  //   //setState(() {});
-  // }
-
-  Future<Uint8List> loadFromAsset(String key) async {
-    final ByteData byteData = await rootBundle.load(key);
-    return byteData.buffer.asUint8List();
   }
 
   //文字の反映をする
-  void onSetWord() {}
+  void onSetWord() {
+    textOption.addText(
+      EditorText(
+        text: 'うんち！',
+        offset: const Offset(0, 0),
+        fontSizePx: 10,
+        textColor: Colors.black,
+        fontName: '',
+      ),
+    );
+  }
 
   //画像の保存
   void onImageStore() {}
