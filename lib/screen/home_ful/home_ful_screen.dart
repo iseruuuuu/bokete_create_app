9f81d2b5532b861873064a51a86107ea8ddab141
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-10-21 18:22:03 +0900

    fix: 改行を削除

--- lib/screen/home_ful/home_ful_screen.dart
+++ lib/screen/home_ful/home_ful_screen.dart
@@ -118,29 +118,28 @@ class _HomeFulScreenState extends State<HomeFulScreen> {
     if (_controller.text.isEmpty) {
       position = const Offset(0, 0);
     } else if (_controller.text.length == 1) {
-      position = Offset(330, 240);
+      position = const Offset(330, 240);
     } else if (_controller.text.length == 2) {
-      position = Offset(300, 240);
+      position = const Offset(300, 240);
     } else if (_controller.text.length == 3) {
-      position = Offset(270, 240);
+      position = const Offset(270, 240);
     } else if (_controller.text.length == 4) {
-      position = Offset(250, 240);
+      position = const Offset(250, 240);
     } else if (_controller.text.length == 5) {
-      position = Offset(220, 240);
+      position = const Offset(220, 240);
     } else if (_controller.text.length == 6) {
-      position = Offset(190, 240);
+      position = const Offset(190, 240);
     } else if (_controller.text.length == 7) {
-      position = Offset(170, 240);
+      position = const Offset(170, 240);
     } else if (_controller.text.length == 8) {
-      position = Offset(150, 240);
+      position = const Offset(150, 240);
     } else if (_controller.text.length == 9) {
-      position = Offset(130, 240);
+      position = const Offset(130, 240);
     } else if (_controller.text.length == 10) {
-      position = Offset(100, 240);
+      position = const Offset(100, 240);
     }
     textOption.addText(
       EditorText(
-        //offset: const Offset(80, 240),
         offset: position,
         text: _controller.text,
         fontSizePx: size,
@@ -151,10 +150,7 @@ class _HomeFulScreenState extends State<HomeFulScreen> {
     option.outputFormat = const OutputFormat.png();
     option.addOption(textOption);
     final Uint8List u = await getAssetImage();
-    final Uint8List? result = await ImageEditor.editImage(
-      image: u,
-      imageEditorOption: option,
-    );
+    final Uint8List? result = await ImageEditor.editImage(image: u, imageEditorOption: option);
     if (result == null) {
       return;
     }
