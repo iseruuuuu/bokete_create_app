0520b11e50f8d1a4488a88e16c5563ad2077b9c3
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-10-24 18:27:13 +0900

    fix: 使わない画像を削除

--- lib/gen/assets.gen.dart
+++ lib/gen/assets.gen.dart
@@ -4,23 +4,14 @@
 /// *****************************************************
 
 import 'package:flutter/widgets.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:flutter/services.dart';
 
 class $AssetsImagesGen {
   const $AssetsImagesGen();
 
-  AssetGenImage get background =>
-      const AssetGenImage('assets/images/background.png');
   AssetGenImage get bokete => const AssetGenImage('assets/images/bokete.png');
-  SvgGenImage get image => const SvgGenImage('assets/images/image.svg');
-  AssetGenImage get odai => const AssetGenImage('assets/images/odai.png');
   AssetGenImage get title01 => const AssetGenImage('assets/images/title01.JPG');
   AssetGenImage get title02 => const AssetGenImage('assets/images/title02.JPG');
   AssetGenImage get title03 => const AssetGenImage('assets/images/title03.JPG');
-  AssetGenImage get title1 => const AssetGenImage('assets/images/title1.JPG');
-  AssetGenImage get title2 => const AssetGenImage('assets/images/title2.JPG');
-  AssetGenImage get title3 => const AssetGenImage('assets/images/title3.JPG');
 }
 
 class Assets {
@@ -76,49 +67,4 @@ class AssetGenImage extends AssetImage {
   }
 
   String get path => assetName;
-}
-
-class SvgGenImage {
-  const SvgGenImage(this._assetName);
-
-  final String _assetName;
-
-  SvgPicture svg({
-    Key? key,
-    bool matchTextDirection = false,
-    AssetBundle? bundle,
-    String? package,
-    double? width,
-    double? height,
-    BoxFit fit = BoxFit.contain,
-    AlignmentGeometry alignment = Alignment.center,
-    bool allowDrawingOutsideViewBox = false,
-    WidgetBuilder? placeholderBuilder,
-    Color? color,
-    BlendMode colorBlendMode = BlendMode.srcIn,
-    String? semanticsLabel,
-    bool excludeFromSemantics = false,
-    Clip clipBehavior = Clip.hardEdge,
-  }) {
-    return SvgPicture.asset(
-      _assetName,
-      key: key,
-      matchTextDirection: matchTextDirection,
-      bundle: bundle,
-      package: package,
-      width: width,
-      height: height,
-      fit: fit,
-      alignment: alignment,
-      allowDrawingOutsideViewBox: allowDrawingOutsideViewBox,
-      placeholderBuilder: placeholderBuilder,
-      color: color,
-      colorBlendMode: colorBlendMode,
-      semanticsLabel: semanticsLabel,
-      excludeFromSemantics: excludeFromSemantics,
-      clipBehavior: clipBehavior,
-    );
-  }
-
-  String get path => _assetName;
 }
